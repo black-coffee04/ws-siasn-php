d2cdfdfb2ff67ae9a3b5619667a08b522aabf762
black-coffee04 <bkdegy@gmail.com>
2024-07-09 09:55:39 +0700

    Add Doc Block Diklat Service

--- src/Services/DiklatService.php
+++ src/Services/DiklatService.php
@@ -48,47 +48,78 @@ class DiklatService implements ServiceInterface
     public function __construct(AuthenticationService $authentication, Config $config)
     {
         $this->authentication = $authentication;
-        $this->config         = $config;
-        $this->httpClient     = new HttpClient($this->config->getApiBaseUrl());
+        $this->config = $config;
+        $this->httpClient = new HttpClient($this->config->getApiBaseUrl());
     }
 
+    /**
+     * Mendapatkan data riwayat diklat berdasarkan ID.
+     *
+     * @param string $idRiwayatDiklat ID riwayat diklat.
+     * @return array Data riwayat diklat.
+     */
     public function get(string $idRiwayatDiklat): array
     {
-        $response = $this->httpClient->get("/apisiasn/1.0/diklat/id/{$idRiwayatDiklat}", [
-            "headers" => $this->getHeaders()
-        ]);
-        
-        return isset($response['data']) && is_array($response['data']) ? $response['data'] : [];
+        $response = $this->httpClient->get(
+            "/apisiasn/1.0/diklat/id/{$idRiwayatDiklat}", 
+            ['headers' => $this->getHeaders()]
+        );
+
+        return $response['data'] ?? [];
     }
 
+    /**
+     * Membuat instance DiklatService dengan data yang diformat.
+     *
+     * @param array $data Data yang akan diformat dan disimpan.
+     * @return self
+     */
     public function create(array $data): self
     {
         $this->data = $this->formatDates($data, ['tanggal', 'tanggalSelesai']);
         return $this;
     }
 
+    /**
+     * Menyertakan dokumen dalam permintaan.
+     *
+     * @param mixed $file Dokumen yang akan diunggah.
+     * @return self
+     */
     public function includeDokumen($file): self
     {
         $dokumenService = new DokumenService($this->authentication, $this->config);
-        $this->dokumen  = $dokumenService->upload($this->idRefDokumenDiklat, $file);
+        $this->dokumen = $dokumenService->upload($this->idRefDokumenDiklat, $file);
         return $this;
     }
 
+    /**
+     * Menyimpan data riwayat diklat.
+     *
+     * @return string ID riwayat diklat yang disimpan atau pesan kesalahan.
+     */
     public function save(): string
     {
-        if ($this->dokumen !== null && is_array($this->dokumen)) {
+        if ($this->dokumen !== null) {
             $this->data['path'] = [$this->dokumen];
         }
 
-        $response = $this->httpClient->post("/apisiasn/1.0/diklat/save", [
-            'json'    => $this->data,
-            'headers' => $this->getHeaders()
-        ]);
+        $response = $this->httpClient->post(
+            "/apisiasn/1.0/diklat/save", 
+            ['json' => $this->data, 'headers' => $this->getHeaders()]
+        );
 
         return $response['mapData']['rwDiklatId'] ?? $response['message'];
     }
 
-    public function remove(string $idRiwayatDiklat)
+    /**
+     * Menghapus riwayat diklat berdasarkan ID.
+     *
+     * @param string $idRiwayatDiklat ID riwayat diklat yang akan dihapus.
+     * @return bool Status keberhasilan penghapusan.
+     * @throws SiasnDataException Jika riwayat diklat tidak ditemukan.
+     */
+    public function remove(string $idRiwayatDiklat): bool
     {
         $diklat = $this->get($idRiwayatDiklat);
 
@@ -96,13 +127,21 @@ class DiklatService implements ServiceInterface
             throw new SiasnDataException('Riwayat diklat tidak ditemukan.');
         }
 
-        $response = $this->httpClient->delete("/apisiasn/1.0/diklat/delete/{$idRiwayatDiklat}", [
-            'headers' => $this->getHeaders()
-        ]);
+        $response = $this->httpClient->delete(
+            "/apisiasn/1.0/diklat/delete/{$idRiwayatDiklat}", 
+            ['headers' => $this->getHeaders()]
+        );
 
         return $response['success'] ?? false;
     }
 
+    /**
+     * Memformat tanggal dalam data.
+     *
+     * @param array $data Data yang akan diformat.
+     * @param array $dateKeys Kunci tanggal yang akan diformat.
+     * @return array Data yang sudah diformat.
+     */
     private function formatDates(array $data, array $dateKeys): array
     {
         foreach ($dateKeys as $key) {
@@ -122,8 +161,8 @@ class DiklatService implements ServiceInterface
     {
         return [
             'Authorization' => 'Bearer ' . $this->getWsoAccessToken(),
-            'Auth'          => 'bearer ' . $this->getSsoAccessToken(),
-            'Accept'        => 'application/json'
+            'Auth' => 'bearer ' . $this->getSsoAccessToken(),
+            'Accept' => 'application/json',
         ];
     }
 
